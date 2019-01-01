8fcd728ecab1bc4851f2c6005e6eeaf5648608e5
Jason Bock <jason@jasonbock.net>
2019-01-01 01:14:33 -0600

    Updated program files to include Whenever text.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WSharp (pronounced "dub Sharp")
-A Whenever compiler written in C#.
+A Whenever compiler written in C#. You can learn more about the language [here](http://www.dangermouse.net/esoteric/whenever.html).
--- WSharp/NinetyNineBottlesGenerator.cs
+++ WSharp/NinetyNineBottlesGenerator.cs
@@ -6,6 +6,12 @@ namespace WSharp
 {
 	internal static class NinetyNineBottlesGenerator
 	{
+		/*
+1 defer (4 || N(1)<N(2) || N(2)<N(3)) print(N(1)+" bottles of beer on the wall, "+N(1)+" bottles of beer,");
+2 defer (4 || N(1)==N(2)) print("Take one down and pass it around,");
+3 defer (4 || N(2)==N(3)) print(N(1)+" bottles of beer on the wall.");
+4 1#98,2#98,3#98; 
+ 		*/
 		internal static ImmutableList<Line> Generate()
 		{
 			var builder = ImmutableList.CreateBuilder<Line>();
